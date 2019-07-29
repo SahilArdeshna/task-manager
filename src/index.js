fb769a817b2e8e378d68fa085f491bcba9aef78c
Sahil Ardeshna <47632549+SahilArdeshna@users.noreply.github.com>
2019-07-29 17:15:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,29 +6,6 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT;
 
-const multer = require('multer');
-
-const upload = multer({
-    dest: 'image',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb) {
-
-        if (!file.originalname.match(/\.(doc|docx)$/)) {
-            return cb(new Error('Please provide Word Document'));
-        }
-
-        cb(undefined, true);
-    }
-});
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send();
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message });
-});
-
 // It parses incoming requests with JSON payloads and is based on body-parser. Returns middleware that only parses JSON.
 app.use(express.json());  
 
